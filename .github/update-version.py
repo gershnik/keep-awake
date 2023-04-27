a3a66fe50eca09e2a1be5279a30276ace4d36773
gershnik <gershnik@hotmail.com>
2023-04-26 23:50:04 -0700

    More work on publishing

--- .github/update-version.py
+++ .github/update-version.py
@@ -5,28 +5,35 @@ import re
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('type', choices=['minor', 'major'])
+parser.add_argument('type', choices=['minor', 'major', 'patch'])
 parser.add_argument('--dry-run', dest='dryRun', action='store_true')
 args = parser.parse_args()
 
 mydir = Path(__file__).parent
 
-def updateVersion(major, minor):
-    if args.type == 'minor':
+def updateVersion(major, minor, patch):
+    if args.type == 'patch':
+        patch = int(patch) + 1
+    elif args.type == 'minor':
         minor = int(minor) + 1
+        patch = 0
     else:
         major = int(major) + 1
         minor = 0
-    return (major, minor)
+        patch = 0
+    return (major, minor, patch)
 
 
 with open('version.json', 'r') as verfile:
     current = json.load(verfile)
 
 
-major, minor = updateVersion(current["BUILD_MAJOR_VERSION"], current["BUILD_MINOR_VERSION"])
+major, minor, patch = updateVersion(current["BUILD_MAJOR_VERSION"], 
+                                    current["BUILD_MINOR_VERSION"],
+                                    current["BUILD_PATCH_VERSION"])
 current["BUILD_MAJOR_VERSION"] = major
 current["BUILD_MINOR_VERSION"] = minor
+current["BUILD_PATCH_VERSION"] = patch
 
 
 if not args.dryRun:
@@ -34,6 +41,6 @@ if not args.dryRun:
         json.dump(current, verfile, indent=4)
 
 
-print(f'{major}.{minor}')
+print(f'{major}.{minor}.{patch}')
 
 
